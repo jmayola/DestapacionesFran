00b076639d632671f4e8a23d47b5288baeebd4d3
Julian Mayola <julianmayola131@gmail.com>
2024-02-10 15:00:25 -0300

    añadidos apartados de contacto y nosotros cambios en los estilos añadido responsive en moviles en vertical añadidos imagenes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors:{
+			'white': '#FEFCFC',
+			'black': '#020000',
+			'red-simple': '#8C181A',
+			'red-darker': '#550200',
+			'gray-darker': '#1A1A1A',
+			'gray-light': '#D9D9D9',
+			'gray-blue': '#A6ADCA',
+			'blue-light': '#18608C',
+		},
 		extend: {},
 	},
 	plugins: [],
